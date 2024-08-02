d3e6e2b74f198e7511f3952458e7b357ad2c7e64
Dennis Schmalstieg <darianbellentor@outlook.de>
2024-08-02 21:17:47 +0200

    player move

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed = 10f;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float hor = Input.GetAxis("Horizontal") * speed * Time.deltaTime;
+        float ver = Input.GetAxis ("Vertical") * speed * Time.deltaTime;
+        transform.position += new Vector3(hor, ver, 0f);    }
+}
